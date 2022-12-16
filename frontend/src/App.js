df3312a3d4f6bafa7453bf3ab95772edda6d3dc2
Ezio-Sarthak <grgsrthk.dev20@gmail.com>
2022-12-16 15:34:48 +0530

    Add Chat component to the project
    This commit uses and integrates Stream Chat component
    in the project.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
-
-import React from 'react';
-import './App.css';
-import Navbar from './components/nav/Navbar'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Feed from './components/pages/Feed';
-import Projects from './components/pages/Projects';
-import Profile from './components/pages/Profile';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/nav/Navbar";
+import MyChat from "./components/MyChat";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Feed from "./components/pages/Feed";
+import Projects from "./components/pages/Projects";
+import Profile from "./components/pages/Profile";
 const App = () => {
   return (
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/Feed' component={Feed} />
-          <Route path='/Projects' component={Projects} />
-          <Route path='/Profile' component={Profile} />
-        </Routes>
-      </Router>
-  )
-}
+    <Router>
+      <Navbar />
+      <MyChat />
+      <Routes>
+        <Route path="/Feed" component={Feed} />
+        <Route path="/Projects" component={Projects} />
+        <Route path="/Profile" component={Profile} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
