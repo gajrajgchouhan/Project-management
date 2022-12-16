6b451252e3031658fe1c4d666ad4753daa9d8818
ShreyaGarg01 <shreyagarg0112@gmail.com>
2022-12-16 15:42:00 +0530

    Login/SignUp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// // import logo from './logo.svg';
+// import './App.css';
+// import Login from './components/Login';
+// // import Signup from './pages/Signup';
+
+// import React from 'react' 
+// import FileRoutes from './Routes';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Login />
+//     </div>
+    
+//   );
+// }
+
+// export default App;
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login />
     </div>
+    
   );
 }
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/login" component={Login} />
+//         <Route path="/signup" component={SignUp} />
+//         <Navigate from="/" to="/signup" />
+//       </Routes>
+//     </>
+//   );
+
 
 export default App;
