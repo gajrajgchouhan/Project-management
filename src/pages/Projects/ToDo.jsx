1b4fcf65bad4c4d46f7f43020b4cdf9ec6d0db21
Gajraj Singh Chouhan <gajrajgchouhan@gmail.com>
2022-12-18 17:37:47 +0530

    Tasks CRUD

--- src/pages/Projects/ToDo.jsx
+++ src/pages/Projects/ToDo.jsx
@@ -1,106 +1,91 @@
-import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import React from "react";
+import Form from "react-bootstrap/Form";
 
-import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faCircleCheck, faPen, faTrashCan
-} from '@fortawesome/free-solid-svg-icons'
+    faCircleCheck,
+    faPen,
+    faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
 
+const ChooseUser = ({ members, assigned_to }) => {
+    const [selected, setSelected] = useState(assigned_to);
+    return (
+        <Form.Select
+            value={selected}
+            aria-label="Choose collaborator"
+            disabled={false}
+            onChange={(e) => setSelected(e.target.value)}
+        >
+            <option>{assigned_to}</option>
+            {members.map((member) => {
+                return <option>{member.username}</option>;
+            })}
+        </Form.Select>
+    );
+};
 
-const ChooseUser = () => {
+const ToDo = ({ toDo, markDone, setUpdateData, updateData, members }) => {
+    console.log("TODO", toDo);
+    return (
+        toDo &&
+        toDo
+            .sort((a, b) =>
+                new Date(a.updatedAt) < new Date(b.updatedAt) ? 1 : -1
+            )
+            .map((task, index) => {
+                return (
+                    <React.Fragment key={task.id}>
+                        <div className="col taskBg">
+                            <div className={task.complete ? "done" : ""}>
+                                <span className="taskNumber">{index + 1}</span>
+                                <span className="taskText">{task.name}</span>
+                            </div>
+                            <span className="assignment">
+                                Created by: &nbsp;&nbsp;&nbsp;&nbsp;
+                                {task.created_by.username}
+                            </span>
+                            <br></br>
+                            <span className="assignment">
+                                {updateData ? (
+                                    <ChooseUser
+                                        {...{
+                                            members,
+                                            assigned_to:
+                                                task.assigned_to?.username,
+                                        }}
+                                    />
+                                ) : (
+                                    <span>
+                                        Assigned to: &nbsp;&nbsp;&nbsp;&nbsp;
+                                        {task.assigned_to?.username}
+                                    </span>
+                                )}
+                            </span>
 
-  const [isDisabled, setIsDisabled] = useState(true);
-  const handleClick = () => {
-    setIsDisabled(!isDisabled)
-  };
-  return (
-    <>
-      <Form.Select aria-label="Choose collaborator" disabled={isDisabled}>
-        <option>Choose collaborator</option>
-        <option value="1">User1</option>
-        <option value="2">User2</option>
-        <option value="3">User3</option>
-      </Form.Select>
-      <button onClick={handleClick} className="btn btn-lg">
-        <FontAwesomeIcon icon={faPen} />
+                            <div className="iconsWrap">
+                                <span
+                                    title="Completed / Not Completed"
+                                    onClick={(e) => markDone(task._id, index)}
+                                >
+                                    <FontAwesomeIcon icon={faCircleCheck} />
+                                </span>
 
-      </button >
-    </>
-
-  );
-}
-
-const ToDo = ({ toDo, markDone, setUpdateData, deleteTask }) => {
-  return (
-    <>
-      {toDo && toDo
-        .sort((a, b) => a.id > b.id ? 1 : -1)
-        .map((task, index) => {
-          return (
-            <React.Fragment key={task.id}>
-
-              <div className="col taskBg">
-                <div className={task.status ? 'done' : ''}>
-                  <span className="taskNumber">{index + 1}</span>
-                  <span className="taskText">{task.title}</span>
-                </div>
-                <span className='assignment'>Created by: &nbsp;&nbsp;&nbsp;&nbsp; User1</span>
-                <br></br>
-
-                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-                  <Form.Label column sm="2" id='assigned'>
-                    Assigned to:
-                  </Form.Label>
-                  <Col sm="8">
-                    <ChooseUser />
-
-
-                    {/* <Form.Select aria-label="Choose collaborator" disabled="true" value={assigned-to}>
-                      <option>Choose collaborator</option>
-                      <option value="1">User1</option>
-                      <option value="2">User2</option>
-                      <option value="3">User3</option>
-                    </Form.Select> */}
-                  </Col>
-                </Form.Group>
-
-                <div className="iconsWrap">
-                  <span title="Completed / Not Completed"
-                    onClick={(e) => markDone(task.id)}
-                  >
-                    <FontAwesomeIcon icon={faCircleCheck} />
-                  </span>
-
-                  {task.status ? null : (
-                    <span title="Edit"
-                      onClick={() => setUpdateData({
-                        id: task.id,
-                        title: task.title,
-                        status: task.status ? true : false,
-                      }
-                      )
-                      }
-                    >
-                      <FontAwesomeIcon icon={faPen} />
-                    </span>
-                  )}
-
-                  <span title="Delete"
-                    onClick={() => deleteTask(task.id)}
-                  >
-                    <FontAwesomeIcon icon={faTrashCan} />
-                  </span>
-                </div>
-              </div>
-            </React.Fragment>
-          )
-        })
-      }
-    </>
-  )
-}
+                                {task.complete ? null : (
+                                    <span
+                                        title="Edit"
+                                        onClick={() => setUpdateData(true)}
+                                    >
+                                        <FontAwesomeIcon icon={faPen} />
+                                    </span>
+                                )}
+                            </div>
+                        </div>
+                    </React.Fragment>
+                );
+            })
+    );
+};
 
 export default ToDo;
