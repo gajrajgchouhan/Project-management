dcb617736797a99ec6c3f13c87697e9e19662ddd
Gajraj Singh Chouhan <gajrajgchouhan@gmail.com>
2022-12-18 11:30:42 +0530

    Fixed footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
     return (
-        <div style={{
-            position: "fixed",
-            paddingTop: "8px",
-            
-            bottom: 0,
-            width: "100%",
-            /* Height of the footer*/
-            height: "12%",
-            backgroundColor: "rgb(45, 52, 70)"
-        }}>
-                <p style={{
+        <div
+            style={{
+                paddingTop: "8px",
+                bottom: 0,
+                width: "100%",
+                /* Height of the footer*/
+                height: "12%",
+                backgroundColor: "rgb(45, 52, 70)",
+            }}
+        >
+            <p
+                style={{
                     color: "white",
                     textAlign: "center",
                     // marginTop: "-3px"
-
-                }}>
-                    <h5>
-                    <a href='/' style={{
-                    color: "rgb(201, 212, 243)",}}>
-                    DevColab
-					</a> , 2022</h5> 
-                    <h6>&copy; All rights reserved</h6>
-                </p>
+                }}
+            >
+                <h5>
+                    <a
+                        href="/"
+                        style={{
+                            color: "rgb(201, 212, 243)",
+                        }}
+                    >
+                        DevColab
+                    </a>{" "}
+                    , 2022
+                </h5>
+                <h6>&copy; All rights reserved</h6>
+            </p>
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
